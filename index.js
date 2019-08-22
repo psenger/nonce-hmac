739f21e3bd1ed006a23d1cca83d5ddce45bdb3fc
Philip A Senger <philip.a.senger@cngrgroup.com>
2019-08-22 12:35:27 +1000

    Update index.js
    Optimized for performance

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const crypto = require('crypto');
 module.exports = ( { key = '', nonce = '', payload = ''} ) => {
   for (const i of [key, nonce, payload]) {
     if (!i) throw new Error('Key, nonce and payload are required.')
     if (typeof i !== "string") throw new TypeError('Key, nonce and payload must be strings.')
   }
-  return require('crypto').createHmac('sha256', key).update(nonce + payload).digest('hex')
+  return crypto.createHmac('sha256', key).update(nonce + payload).digest('hex')
 }
